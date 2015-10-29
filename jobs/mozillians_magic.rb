48dc2295315cd64d1b4b4407f6e85f7e7c735205
Leo McArdle <leomcardle@gmail.com>
2015-10-29 12:29:18 +0000

    get members of nda group from API https://discourse.mozilla-community.org/t/discourse-mozillians-get-members-of-nda-group-from-api/4913

--- jobs/mozillians_magic.rb
+++ jobs/mozillians_magic.rb
@@ -1,30 +1,62 @@
 module Jobs
   class MozilliansMagic < Jobs::Base
 
-    def add_to_group(user, group_name)
+    def add_to_group(group_name)
       group = Group.where(name: group_name).first
       if group
-        if group.group_users.where(user_id: user.id).first.nil?
-          group.group_users.create(user_id: user.id, group_id: group.id)
+        if group.group_users.where(user_id: @user.id).first.nil?
+          group.group_users.create(user_id: @user.id, group_id: group.id)
         end
       end
     end
 
-    def remove_from_group(user, group_name)
+    def remove_from_group(group_name)
       group = Group.where(name: group_name).first
       if group
-        if not group.group_users.where(user_id: user.id).first.nil?
-          group.group_users.where(user_id: user.id).destroy_all
+        if not group.group_users.where(user_id: @user.id).first.nil?
+          group.group_users.where(user_id: @user.id).destroy_all
         end
       end
     end
 
-    def purge_from_groups(user)
-      remove_from_group(user, 'mozillians')
+    def purge_from_groups
+      remove_from_group('mozillians')
 
       groups = Group.where("name LIKE 'mozillians*_%' ESCAPE '*'")
       groups.each do |group|
-        remove_from_group(user, group.name)
+        remove_from_group(group.name)
+      end
+    end
+
+  def query_mozillians(params = {})
+      uri = URI('https://mozillians.org/api/v2/users/')
+      params = { email: @user.email }.merge(params)
+      uri.query = URI.encode_www_form(params)
+
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+
+      request = Net::HTTP::Get.new(uri.request_uri)
+      request['X-API-KEY'] = SiteSetting.mozillians_api_key
+
+      http.request(request)
+    end
+
+    def query_mozillians_group(group)
+      response = query_mozillians({ group: group })
+
+      if response.code.to_i == 200
+        res = JSON.parse(response.body)
+        count = res['count']
+
+        if count.to_i == 1
+          add_to_group("mozillians_#{group}")
+        else
+          remove_from_group("mozillians_#{group}")
+        end
+
+      else
+        purge_from_groups
       end
     end
 
@@ -33,23 +65,12 @@ module Jobs
 
       return unless SiteSetting.mozillians_enabled?
 
-      user = User.find(args[:user_id])
-      email = user.email
+      @user = User.find(args[:user_id])
 
-      Rails.logger.info "Mozillians: getting data for #{email}"
+      Rails.logger.info "Mozillians: getting data for #{@user.email}"
 
       begin
-        uri = URI('https://mozillians.org/api/v2/users/')
-        params = { email: email }
-        uri.query = URI.encode_www_form(params)
-
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true
-
-        request = Net::HTTP::Get.new(uri.request_uri)
-        request['X-API-KEY'] = SiteSetting.mozillians_api_key
-
-        response = http.request(request)
+        response = query_mozillians
 
         if response.code.to_i == 200
           res = JSON.parse(response.body)
@@ -58,27 +79,28 @@ module Jobs
           if count.to_i == 1
             is_vouched = !!res['results'].first['is_vouched']
 
-            add_to_group(user, 'mozillians')
+            add_to_group('mozillians')
 
             if is_vouched
-              remove_from_group(user, 'mozillians_unvouched')
-              add_to_group(user, 'mozillians_vouched')
+              remove_from_group('mozillians_unvouched')
+              add_to_group('mozillians_vouched')
+              query_mozillians_group('nda')
             else
-              remove_from_group(user, 'mozillians_vouched')
-              add_to_group(user, 'mozillians_unvouched')
+              remove_from_group('mozillians_vouched')
+              add_to_group('mozillians_unvouched')
             end
 
           else
-            purge_from_groups(user)
+            purge_from_groups
           end
 
         else
-          purge_from_groups(user)
+          purge_from_groups
         end
 
       rescue Exception => details
         Rails.logger.error "Failed to query API: #{details.message}"
-        purge_from_groups(user)
+        purge_from_groups
       end
     end
 
