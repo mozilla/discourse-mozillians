c46ad21c46267df268bff36884d8d056d669d65a
Leo McArdle <leomcardle@gmail.com>
2016-11-21 21:35:32 +0000

    add setting to enable/disable daily job

--- jobs/scheduled/mozillians_refresh.rb
+++ jobs/scheduled/mozillians_refresh.rb
@@ -4,6 +4,7 @@ module Jobs
 
     def execute(args)
       return unless SiteSetting.mozillians_enabled?
+      return unless SiteSetting.mozillians_refresh_enabled?
 
       staff_action_logger = StaffActionLogger.new(Discourse.system_user)
       staff_action_logger.log_custom('mozillians_refresh')
